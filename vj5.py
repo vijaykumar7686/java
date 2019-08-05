ce22ab2272c3acf7b8902d0aceca696e70ac9131
vijaykumar7686 <53746454+vijaykumar7686@users.noreply.github.com>
2019-08-05 14:40:59 +0530

    Update vj5.py

--- vj5.py
+++ vj5.py
@@ -1,10 +1,10 @@
-a=int(input())
+a1=int(input())
 b=int(input())
 c=int(input())
 
-if(a>b) :
-    if(a>c):
-        print(a)
+if(a1>b) :
+    if(a1>c):
+        print(a1)
     else:
         print(c)
 elif(b>c):
